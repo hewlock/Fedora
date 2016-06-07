90d82d6c676502c5e1f751baeffc9aedc3538770
Matthew Moulton <matthew.moulton@gmail.com>
2016-06-07 18:03:23 -0400

    add gnome arc theme install

--- gnome/install.sh
+++ gnome/install.sh
@@ -3,3 +3,23 @@ if [ "$DESKTOP_SESSION" != "gnome" ]; then
 fi
 
 echo "[INFO] Installing Gnome"
+
+sudo rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
+sudo rm -rf ~/.local/share/themes/{Arc,Arc-Darker,Arc-Dark}
+sudo rm -rf ~/.themes/{Arc,Arc-Darker,Arc-Dark}
+
+if [ "$OS_NAME" = "Fedora" ]; then
+	sudo dnf -y install autoconf
+	sudo dnf -y install automake
+	sudo dnf -y install pkgconfig
+	sudo dnf -y install gtk3-devel
+fi
+
+git clone https://github.com/horst3180/arc-theme --depth 1
+cd arc-theme
+./autogen.sh --prefix=/usr
+sudo make install
+cd ..
+rm -rf arc-theme
+
+gsettings set org.gnome.desktop.interface gtk-theme "Arc"
--- vim/install.sh
+++ vim/install.sh
@@ -4,4 +4,9 @@ if [ "$OS_NAME" = "Fedora" ]; then
 	sudo dnf -y install vim-enhanced
 fi
 
+if [ -e ~/.vimrc ]; then
+	echo "[INFO] removing old ~/.vimrc"
+	rm ~/.vimrc
+fi
+
 ln -s $(pwd)/vim/.vimrc ~/.vimrc
