076c4f0e6cad2f266e7b30171eec048920dacacc
Matthew Moulton <matthew.moulton@gmail.com>
2016-06-11 22:41:04 -0400

    auto-install emacs packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,6 +2,13 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(setq package-list '(color-theme-solarized))
+(unless package-archive-contents
+  (package-refresh-contents))
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
 (setq auto-save-visited-file-name t)
 (setq auto-save-interval 20)
 (setq auto-save-timeout 5)
