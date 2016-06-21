a29f13f848ea7c6fb178ac624739055c850c0a29
Matthew Moulton <matthew.moulton@gmail.com>
2016-06-21 19:19:43 -0400

    add gnome git credential store

---
+++ git/install.sh
@@ -0,0 +1,8 @@
+if [ "$DESKTOP_SESSION" != "gnome" ] || [ "$OS_NAME" != "Fedora" ]; then
+	exit 0
+fi
+
+echo -e "[\033[0;32mINFO\033[0m] install git"
+
+git config --global credential.helper gnome-keyring
+
