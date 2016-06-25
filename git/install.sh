86b05ab12e21de6be3f8d08dbd5b364c628a90cb
Matthew Moulton <matthew.moulton@gmail.com>
2016-06-25 13:26:49 -0400

    setup default git username/email

--- git/install.sh
+++ git/install.sh
@@ -6,3 +6,5 @@ echo -e "[\033[0;32mINFO\033[0m] install git"
 
 git config --global alias.co checkout
 git config --global credential.helper gnome-keyring
+git config --global user.email matthew.moulton@gmail.com
+git config --global user.name "Matthew Moulton"
