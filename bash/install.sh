e72472e4a655761af2072333279e53c2faf64fba
Matthew Moulton <matthew.moulton@gmail.com>
2016-07-05 20:16:31 -0400

    refactor package installation

--- bash/install.sh
+++ bash/install.sh
@@ -13,8 +13,9 @@ fi
 echo -e "[\033[0;32mINFO\033[0m] link ~/.bashrc"
 ln -s $(pwd)/bash/.bashrc ~/.bashrc
 
-echo -e "[\033[0;32mINFO\033[0m] install powerline"
-sudo dnf -y install powerline
+echo -e "[\033[0;32mINFO\033[0m] install rpm repositories as root"
+su -c "curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -"
+
 if [ -e ~/.config/powerline ]; then
 	echo -e "[\033[0;32mINFO\033[0m] remove ~/.config/powerline"
 	rm ~/.config/powerline
@@ -22,17 +23,8 @@ fi
 echo -e "[\033[0;32mINFO\033[0m] link ~/.config/powerline"
 ln -s $(pwd)/bash/powerline ~/.config/powerline
 
-echo -e "[\033[0;32mINFO\033[0m] install ack"
-sudo dnf -y install ack
-
-echo -e "[\033[0;32mINFO\033[0m] install acpi"
-sudo dnf -y install acpi
-
-echo -e "[\033[0;32mINFO\033[0m] install fortune"
-sudo dnf -y install fortune-mod
-
-echo -e "[\033[0;32mINFO\033[0m] install rpm repositories as root"
-su -c "curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -"
-
-echo -e "[\033[0;32mINFO\033[0m] install node"
-sudo dnf -y install nodejs
+for PACKAGE in ack acpi fortune-mod powerline nodejs; do
+	echo ""
+	echo -e "[\033[0;32mINFO\033[0m] install ${PACKAGE}"
+	sudo dnf -y install ${PACKAGE}
+done
